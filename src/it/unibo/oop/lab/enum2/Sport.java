4d18c42cb7454548cf8e746dc5833e39fa9ff725
mega2799 <megadigimatt@gmail.com>
2020-11-14 17:54:11 +0100

    Completed the enum2, needs a code rewiew

--- src/it/unibo/oop/lab/enum2/Sport.java
+++ src/it/unibo/oop/lab/enum2/Sport.java
@@ -16,69 +16,50 @@ package it.unibo.oop.lab.enum2;
  * 
  */
 public enum Sport {
+	
+    BASKET(Place.INDOOR,5,"basket"),
+    TENNIS(Place.INDOOR,1,"tennis"),
+    VOLLEY(Place.INDOOR,8,"volley"),
+    BIKE(Place.OUTDOOR,1,"bike"),
+    F1(Place.OUTDOOR,1,"f1"),
+    MOTOGP(Place.INDOOR,1,"motogp"),
+    SOCCER(Place.INDOOR,11,"soccer");
 
-    /*
-     * TODO
-     * 
-     * Declare the following sports:
-     * 
-     * - basket
-     * 
-     * - volley
-     * 
-     * - tennis
-     * 
-     * - bike
-     * 
-     * - F1
-     * 
-     * - motogp
-     * 
-     * - soccer
-     * 
-     */
+    private final Place place; 
+	private int noTeamMembers;
+	private String actualName; 
+	/**
+	 * @param place noTeamMembers actualName 
+	 *
+	 *
+	 * */
+	Sport(final Place place, final int noTeamMembers, final String actualName) {
+		this.place = place;
+        this.noTeamMembers = noTeamMembers;
+        this.actualName = actualName;
+	}
+	/**
+	 * @return {@link Boolean} 
+	 *			returns true if the sport is an individual sport
+	 *
+	 * */
+	public boolean isIndividualSport(){
+		return this.noTeamMembers == 1;	
+	}
+	
+	
+	public boolean isIndoorSport(){
+		return this.place == Place.INDOOR;
+	}
 
-    /*
-     * TODO
-     * 
-     * [FIELDS]
-     * 
-     * Declare required fields
-     */
-
-    /*
-     * TODO
-     * 
-     * [CONSTRUCTOR]
-     * 
-     * Define a constructor like this:
-     * 
-     * - Sport(final Place place, final int noTeamMembers, final String actualName)
-     */
-
-    /*
-     * TODO
-     * 
-     * [METHODS] To be defined
-     * 
-     * 
-     * 1) public boolean isIndividualSport()
-     * 
-     * Must return true only if called on individual sports
-     * 
-     * 
-     * 2) public boolean isIndoorSport()
-     * 
-     * Must return true in case the sport is practices indoor
-     * 
-     * 
-     * 3) public Place getPlace()
-     * 
-     * Must return the place where this sport is practiced
-     * 
-     * 
-     * 4) public String toString()
-     * 
-     * Returns the string representation of a sport
-     */
+	
+	public Place getPlace(){
+		return this.place;
+	}
+	
+	public String toString() {
+		return "place:" + this.place + "team: " + this.noTeamMembers + 
+				"actual name: " + this.actualName;
+	}
+    
 }
--- src/it/unibo/oop/lab/enum2/Sport2SocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/enum2/Sport2SocialNetworkUserImpl.java
@@ -3,6 +3,11 @@
  */
 package it.unibo.oop.lab.enum2;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+import it.unibo.oop.lab.enum2.Sport;
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUserImpl;
 import it.unibo.oop.lab.socialnetwork.User;
 
@@ -30,7 +35,7 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      * 
      * add a field to keep track of the set of sports followed/done by a user
      */
-
+	private final Set<Sport> sports = new HashSet<>();
     /**
      * Builds a new {@link Sport2SocialNetworkUserImpl}.
      * 
@@ -75,7 +80,7 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      *            a sport followed/done by the user
      */
     public void addSport(final Sport sport) {
-
+    	this.sports.add(sport);
     }
 
     /**
@@ -87,7 +92,7 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      * @return true if user likes sport s
      */
     public boolean likesSport(final Sport s) {
-        return false;
+        return this.sports.contains(s);
     }
 
     /*
@@ -100,18 +105,33 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      * 
      * @return the set of individual sport this user practices/follows
      */
-    /*
-     * public Set<Sport> getIndividualSports() { return null; }
-     * 
-     * 
-     * /** Returns the set of sports which are practiced in a given place.
-     * 
-     * @param p the place in which the sport is practiced in order to be
-     * included in the resulting set
-     * 
-     * @return the set of sport practiced in a given place
+	public Set<Sport> getIndividualSports(){
+		final Set<Sport> individualSport = new TreeSet<>();
+
+		for( Sport s : this.sports){	
+			if ( s.isIndividualSport() ) {
+				individualSport.add(s);
+			}
+		}
+		return individualSport;
+	}
+    
+      
+      /** Returns the set of sports which are practiced in a given place.
+      *
+      *@param p the place in which the sport is practiced in order to be
+      *included in the resulting set
+      *
+      *@return the set of sport practiced in a given place
      */
-    /*
-     * public Set<Sport> getSportPracticedInPlace(Place p) { return null; }
-     */
+      public Set<Sport> getSportPracticedInPlace(Place p) { 
+    	  final Set<Sport> locatedSport = new TreeSet<>();
+    	  for( Sport s: this.sports) {
+    		  if (s.getPlace().equals(p)) {
+    			  locatedSport.add(s);
+    		  }
+    	  }
+    	  return locatedSport;
+      }
+ 
 }
